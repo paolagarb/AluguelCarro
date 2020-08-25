26e5c3e793d9bea95f0638ca97ad1c83ec8dc0a1
Paola Garbato <paolagarbato@gmail.com>
2020-08-25 13:32:09 -0300

    Ajustando interface

--- Alguel_Carro/Form1.Designer.cs
+++ Alguel_Carro/Form1.Designer.cs
@@ -34,15 +34,19 @@
             this.label2 = new System.Windows.Forms.Label();
             this.txtModelo = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.pickerRetirada = new System.Windows.Forms.DateTimePicker();
             this.label4 = new System.Windows.Forms.Label();
-            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.button1 = new System.Windows.Forms.Button();
+            this.txtValorHora = new System.Windows.Forms.TextBox();
+            this.txtValorDia = new System.Windows.Forms.TextBox();
+            this.btnConfirmar = new System.Windows.Forms.Button();
+            this.pickerDevolucao = new System.Windows.Forms.DateTimePicker();
+            this.pickerRetiradaH = new System.Windows.Forms.DateTimePicker();
+            this.pickerDevolucaoH = new System.Windows.Forms.DateTimePicker();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
@@ -59,17 +63,18 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Algerian", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(174, 28);
+            this.label1.Location = new System.Drawing.Point(174, 21);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(200, 39);
             this.label1.TabIndex = 1;
             this.label1.Text = "Seu Carro";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(36, 111);
+            this.label2.Location = new System.Drawing.Point(31, 111);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(74, 18);
             this.label2.TabIndex = 2;
@@ -78,56 +83,48 @@
             // txtModelo
             // 
             this.txtModelo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtModelo.Location = new System.Drawing.Point(151, 112);
+            this.txtModelo.Location = new System.Drawing.Point(138, 112);
             this.txtModelo.Name = "txtModelo";
-            this.txtModelo.Size = new System.Drawing.Size(100, 22);
+            this.txtModelo.Size = new System.Drawing.Size(130, 22);
             this.txtModelo.TabIndex = 3;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(36, 149);
+            this.label3.Location = new System.Drawing.Point(31, 149);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(86, 18);
             this.label3.TabIndex = 4;
             this.label3.Text = "Retirada: ";
             // 
-            // dateTimePicker1
+            // pickerRetirada
             // 
-            this.dateTimePicker1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker1.Location = new System.Drawing.Point(151, 149);
-            this.dateTimePicker1.MaxDate = new System.DateTime(2021, 8, 25, 0, 0, 0, 0);
-            this.dateTimePicker1.MinDate = new System.DateTime(2020, 8, 28, 0, 0, 0, 0);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(205, 22);
-            this.dateTimePicker1.TabIndex = 5;
-            this.dateTimePicker1.Value = new System.DateTime(2020, 8, 28, 0, 0, 0, 0);
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            this.pickerRetirada.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pickerRetirada.Location = new System.Drawing.Point(138, 149);
+            this.pickerRetirada.MaxDate = new System.DateTime(2021, 8, 25, 0, 0, 0, 0);
+            this.pickerRetirada.MinDate = new System.DateTime(2020, 8, 28, 0, 0, 0, 0);
+            this.pickerRetirada.Name = "pickerRetirada";
+            this.pickerRetirada.Size = new System.Drawing.Size(205, 22);
+            this.pickerRetirada.TabIndex = 5;
+            this.pickerRetirada.Value = new System.DateTime(2020, 8, 28, 0, 0, 0, 0);
+            this.pickerRetirada.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(36, 187);
+            this.label4.Location = new System.Drawing.Point(31, 187);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(98, 18);
             this.label4.TabIndex = 6;
             this.label4.Text = "Devolução:";
             // 
-            // dateTimePicker2
-            // 
-            this.dateTimePicker2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dateTimePicker2.Location = new System.Drawing.Point(151, 187);
-            this.dateTimePicker2.Name = "dateTimePicker2";
-            this.dateTimePicker2.Size = new System.Drawing.Size(205, 22);
-            this.dateTimePicker2.TabIndex = 7;
-            // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(36, 223);
+            this.label5.Location = new System.Drawing.Point(31, 223);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(96, 18);
             this.label5.TabIndex = 8;
@@ -137,57 +134,102 @@
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(36, 259);
+            this.label6.Location = new System.Drawing.Point(31, 259);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(83, 18);
             this.label6.TabIndex = 9;
             this.label6.Text = "Valor/dia:";
             // 
-            // textBox1
+            // txtValorHora
             // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(151, 224);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 10;
+            this.txtValorHora.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValorHora.Location = new System.Drawing.Point(138, 224);
+            this.txtValorHora.Name = "txtValorHora";
+            this.txtValorHora.Size = new System.Drawing.Size(118, 22);
+            this.txtValorHora.TabIndex = 10;
             // 
-            // textBox2
+            // txtValorDia
             // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(151, 257);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 22);
-            this.textBox2.TabIndex = 11;
+            this.txtValorDia.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtValorDia.Location = new System.Drawing.Point(138, 257);
+            this.txtValorDia.Name = "txtValorDia";
+            this.txtValorDia.Size = new System.Drawing.Size(118, 22);
+            this.txtValorDia.TabIndex = 11;
             // 
-            // button1
+            // btnConfirmar
             // 
-            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
-            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(151, 309);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 30);
-            this.button1.TabIndex = 12;
-            this.button1.Text = "CONFIRMAR";
-            this.button1.UseVisualStyleBackColor = false;
+            this.btnConfirmar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnConfirmar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Gray;
+            this.btnConfirmar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.btnConfirmar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnConfirmar.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirmar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnConfirmar.Location = new System.Drawing.Point(181, 307);
+            this.btnConfirmar.Name = "btnConfirmar";
+            this.btnConfirmar.Size = new System.Drawing.Size(105, 36);
+            this.btnConfirmar.TabIndex = 12;
+            this.btnConfirmar.Text = "CONFIRMAR";
+            this.btnConfirmar.UseVisualStyleBackColor = false;
+            // 
+            // pickerDevolucao
+            // 
+            this.pickerDevolucao.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pickerDevolucao.Location = new System.Drawing.Point(138, 187);
+            this.pickerDevolucao.MaxDate = new System.DateTime(2021, 8, 25, 0, 0, 0, 0);
+            this.pickerDevolucao.MinDate = new System.DateTime(2020, 8, 28, 0, 0, 0, 0);
+            this.pickerDevolucao.Name = "pickerDevolucao";
+            this.pickerDevolucao.Size = new System.Drawing.Size(205, 22);
+            this.pickerDevolucao.TabIndex = 13;
+            this.pickerDevolucao.Value = new System.DateTime(2020, 8, 28, 0, 0, 0, 0);
+            // 
+            // pickerRetiradaH
+            // 
+            this.pickerRetiradaH.AccessibleRole = System.Windows.Forms.AccessibleRole.Clock;
+            this.pickerRetiradaH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pickerRetiradaH.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.pickerRetiradaH.Location = new System.Drawing.Point(349, 149);
+            this.pickerRetiradaH.Name = "pickerRetiradaH";
+            this.pickerRetiradaH.Size = new System.Drawing.Size(68, 22);
+            this.pickerRetiradaH.TabIndex = 14;
+            // 
+            // pickerDevolucaoH
+            // 
+            this.pickerDevolucaoH.AccessibleRole = System.Windows.Forms.AccessibleRole.Clock;
+            this.pickerDevolucaoH.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pickerDevolucaoH.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.pickerDevolucaoH.Location = new System.Drawing.Point(349, 187);
+            this.pickerDevolucaoH.Name = "pickerDevolucaoH";
+            this.pickerDevolucaoH.Size = new System.Drawing.Size(68, 22);
+            this.pickerDevolucaoH.TabIndex = 15;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(404, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(30, 24);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.ClientSize = new System.Drawing.Size(385, 371);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(446, 371);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pickerDevolucaoH);
+            this.Controls.Add(this.pickerRetiradaH);
+            this.Controls.Add(this.pickerDevolucao);
+            this.Controls.Add(this.btnConfirmar);
+            this.Controls.Add(this.txtValorDia);
+            this.Controls.Add(this.txtValorHora);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.dateTimePicker2);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.pickerRetirada);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.txtModelo);
             this.Controls.Add(this.label2);
@@ -198,6 +240,7 @@
             this.Text = "ALUGUE SEU CARRO";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -210,14 +253,17 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtModelo;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker pickerRetirada;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.DateTimePicker dateTimePicker2;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtValorHora;
+        private System.Windows.Forms.TextBox txtValorDia;
+        private System.Windows.Forms.Button btnConfirmar;
+        private System.Windows.Forms.DateTimePicker pickerDevolucao;
+        private System.Windows.Forms.DateTimePicker pickerRetiradaH;
+        private System.Windows.Forms.DateTimePicker pickerDevolucaoH;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
